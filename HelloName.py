e0423ef343850a84a25dcfed96ed28be03b15b0c
Seven <43576956+jujj7474@users.noreply.github.com>
2019-03-17 21:52:51 +0800

    Update HelloName.py

--- HelloName.py
+++ HelloName.py
@@ -1,3 +1,9 @@
 name = input('please input your name:')
 print('Hello,',name)
 
+#print absolute value of an integer:   注释(解释器会忽略)
+a = 100
+if a >= 0:       # ‘:’形成代码块，默认4空格缩进
+  print(a)       # 缩进：复制粘贴功能失效
+else:            # python大小写敏感
+  print(-a)
