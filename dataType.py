0f01169341084a0cec6b85c88cf0cd8038262a7c
Seven <13500079676@163.com>
2019-04-21 00:33:49 +0800

    bool&list
    布尔型&列表增删改查&python列表表达式

--- dataType.py
+++ dataType.py
@@ -23,3 +23,37 @@ print(newCity3.lower())
 
 print(len(city))
 #7
+
+#布尔型
+#'true'  'false'
+
+#列表  ['a',5,25.5]  数据可具备不同数据类型  增删改查
+studentList = ['aa','bb','cc']
+studentList.append('dd')
+studentList.extend(['ee'])
+studentList.insert(2,'qq')
+print(studentList)
+#['aa','bb','cc','dd']  ['aa','bb','cc','ee'] ['aa','bb','qq','cc']
+
+studentList.pop()
+del(studentList[0])
+studentList.remove('bb')
+#['aa','bb'] ['bb','cc'] ['aa','cc']
+
+studentList[0] = 'qq'
+#['qq','bb','cc']
+
+#in(存在)  true/false
+#not in(不存在) 
+
+#python列表表达式
+'''
+从一个数据序列构建一个新的数据序列的方式
+推导式(comprehensions)是python的一种独有特性
+基本语法：[表达式for变量in列表if条件]
+'''
+#筛选长度大于3的字符串列表
+names = ['Jerry','Bob','Tom','Smith']
+select_name = [names.upper() for name in names if len(name)>3]
+print(select_name)
+#['JERRY','SMITH']
