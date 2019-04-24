1bad6634978e12ca2a44ef74a20b1932ee39d0a5
Seven <13500079676@163.com>
2019-04-25 00:22:18 +0800

    IforWhile
    控制流 判断if    运算符 循环for while

---
+++ Iforwhile.py
@@ -0,0 +1,37 @@
+#控制流和运算符 判断if 循环for、while  
+if 10>5:
+print('10>5)'
+
+lang = input('请选择你所输入的语言，es、cn或其他：')
+if lang == 'es':
+	print('Hi')
+elif lang == 'cn':
+	print('您好')
+else:
+	print('Hello')
+
+#for变量in列表或者字符串等，满足则执行
+city = 'beijing'
+for i in city:
+	print(i)
+
+#while条件满足：执行代码
+num = 0
+while num<5:
+	print('num<5')
+	num = num +5
+
+#break结束循环 continue结束本次，开始下一循环
+list1 = [1,2,3,4]
+for i in list1:
+	if i == 3:
+		break
+	print(i)
+
+i=0
+while i<5:
+	i = i+1
+	print('---')
+	if i==3:
+		continue
+	print(i)
